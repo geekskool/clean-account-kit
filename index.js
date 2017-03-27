b4f484749e5b9842d7e7d32ccd41115705419710
ryuzaki644 <sathwikkoushal@gmail.com>
2017-03-27 14:31:47 +0530

    -Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-class IOCore {constructor (ioFunc) {this.then = cb => ioFunc((...args) => { cb(...args) });};reject (pred) {let saveThen = this.then;this.then = cb => {saveThen((...args) => {let result = pred(...args);if (result !== null) {if (Array.isArray(result)) {cb(...result);} else {cb(result);}};});};return this;};mayBeFalse (mv, handler) {let saveThen = this.then;this.then = cb => {saveThen((...args) => {let result = mv(...args);if (result === false) {handler(...args);} else {cb(...args);}});};return this;};mayBeNull (mv, handler) {let saveThen = this.then;this.then = cb => {saveThen((...args) => {let result = mv(...args);if (result === null) {handler(...args);} else {cb(...args);}});};return this;};mayBeErr (mv, handler) {let saveThen = this.then;this.then = cb => {saveThen((...args) => {let result = mv(...args);if (result instanceof Error) {handler(...args);} else {cb(...args);}});};return this;};mayBeTrue (mv, handler) {let saveThen = this.then;this.then = cb => {saveThen((...args) => {let result = mv(...args);if (result === true) {handler(...args);} else {cb(...args);}});};return this;};mayBeUndefined (mv, handler) {let saveThen = this.then;this.then = cb => {saveThen((...args) => {let result = mv(...args);if (result === undefined) {handler(...args);} else {cb(...args);}});};return this;};map (transform) {let saveThen = this.then;this.then = cb => {saveThen((...args) => {let result = transform(...args);if (Array.isArray(result)) {cb(...result);} else {cb(result);}});};return this;};bind (ioFunc) {let saveThen = this.then;this.then = cb => {saveThen((...args) => {let io = ioFunc(...args);io.then((...ioargs) => cb(...args, ...ioargs));});};return this;};static timer (s) {var intervalId;var timer = new IOCore(cb => {intervalId = setInterval(cb, Math.floor(s * 1000))});timer.clear = () => clearInterval(intervalId);return timer;};static createIO (ioFunc) {return new IOCore(ioFunc);};};const readline = require('readline');const fs = require('fs');const rlConfig = {input: process.stdin,output: process.stdout}; class IO extends IOCore {static getLine (str) {const rl = readline.createInterface(rlConfig);return new IOCore(cb => rl.question(str, cb)).map(data => {rl.close();return data;});};static putLine (...data) {return new IOCore(cb => process.nextTick(cb, data)).map(data => {console.log(...data);return data});};static readFile (filename) {return new IOCore(cb => fs.readFile(filename, cb)).map((_, data) => data.toString());};static writeFile (filename, data) {return new IOCore(cb => fs.writeFile(filename, data, cb));};};
+class IOCore {constructor (ioFunc) {this.then = cb => ioFunc((...args) => { cb(...args) });};reject (pred) {let saveThen = this.then;this.then = cb => {saveThen((...args) => {let result = pred(...args);if (result !== null) {if (Array.isArray(result)) {cb(...result);} else {cb(result);}};});};return this;};maybeFalse (mv, handler) {let saveThen = this.then;this.then = cb => {saveThen((...args) => {let result = mv(...args);if (result === false) {handler(...args);} else {cb(...args);}});};return this;};maybeNull (mv, handler) {let saveThen = this.then;this.then = cb => {saveThen((...args) => {let result = mv(...args);if (result === null) {handler(...args);} else {cb(...args);}});};return this;};maybeErr (mv, handler) {let saveThen = this.then;this.then = cb => {saveThen((...args) => {let result = mv(...args);if (result instanceof Error) {handler(...args);} else {cb(...args);}});};return this;};maybeTrue (mv, handler) {let saveThen = this.then;this.then = cb => {saveThen((...args) => {let result = mv(...args);if (result === true) {handler(...args);} else {cb(...args);}});};return this;};maybeUndefined (mv, handler) {let saveThen = this.then;this.then = cb => {saveThen((...args) => {let result = mv(...args);if (result === undefined) {handler(...args);} else {cb(...args);}});};return this;};map (transform) {let saveThen = this.then;this.then = cb => {saveThen((...args) => {let result = transform(...args);if (Array.isArray(result)) {cb(...result);} else {cb(result);}});};return this;};bind (ioFunc) {let saveThen = this.then;this.then = cb => {saveThen((...args) => {let io = ioFunc(...args);io.then((...ioargs) => cb(...args, ...ioargs));});};return this;};static timer (s) {var intervalId;var timer = new IOCore(cb => {intervalId = setInterval(cb, Math.floor(s * 1000))});timer.clear = () => clearInterval(intervalId);return timer;};static createIO (ioFunc) {return new IOCore(ioFunc);};};const readline = require('readline');const fs = require('fs');const rlConfig = {input: process.stdin,output: process.stdout}; class IO extends IOCore {static getLine (str) {const rl = readline.createInterface(rlConfig);return new IOCore(cb => rl.question(str, cb)).map(data => {rl.close();return data;});};static putLine (...data) {return new IOCore(cb => process.nextTick(cb, data)).map(data => {console.log(...data);return data});};static readFile (filename) {return new IOCore(cb => fs.readFile(filename, cb)).map((_, data) => data.toString());};static writeFile (filename, data) {return new IOCore(cb => fs.writeFile(filename, data, cb));};};
 
 const express = require('express');
 const path = require('path');
@@ -20,94 +20,63 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParserJSON);
 app.use(bodyParser.urlencoded({ extended: true }));
-const meEndpointBaseURL = 'https://graph.accountkit.com/' + akConfig.version + '/me';
-const tokenExchangeBaseURL = 'https://graph.accountkit.com/' + akConfig.version + '/access_token';
+const tokenExchangeBaseURL = 'https://graph.accountkit.com/' + akConfig.version + '/access_token?';
+const meEndpointBaseURL = 'https://graph.accountkit.com/' + akConfig.version + '/me' + '?access_token=';
+const params = {
+    grant_type: 'authorization_code',
+    access_token: [
+        'AA',
+        akConfig.appID,
+        akConfig.appSecret
+    ].join('|')
+};
 app.use(sessions({
     cookieName: 'session',
     secret: 'mysecret',
     duration: 7 * 24 * 60 * 60 * 1000,
     activeDuration: 24 * 60 * 60 * 1000
 }));
-IO.createIO(cb => app.get('/', cb)).mayBeUndefined((request, response) => request.session.user, (request, response) => response.redirect('/login')).then((request, response) => {
+IO.createIO(cb => app.get('/', cb)).maybeUndefined((request, response) => request.session.user, (request, response) => response.redirect('/login')).then((request, response) => {
     response.send('hello world');
 });
-IO.createIO(cb => app.get('/login', cb)).bind((request, response, _) => IO.readFile('views/login.html')).then((request, response, _, loginTemplate) => {
+IO.createIO(cb => app.get('/login', cb)).maybeTrue((request, response, _) => !!request.session.user, (request, response, _) => response.redirect('/')).bind((request, response, _) => IO.readFile('views/login.html')).then((request, response, _, loginTemplate) => {
     response.send(Mustache.to_html(loginTemplate, AKINIT));
 });
-IO.createIO(cb => app.post('/login_success', cb)).map((req, res, _) => [
-    req.body.csrf === csrfGuid,
-    req,
-    res,
+IO.createIO(cb => app.post('/login', cb)).map((request, response, _) => [
+    request.body.csrf === csrfGuid,
+    request,
+    response,
     _
-]).mayBeFalse((csrfCheck, req, res, _) => csrfCheck, (csrfCheck, req, res, _) => res.end('Something went terribly wrong')).map((csrfCheck, req, res, _) => [
-    {
-        grant_type: 'authorization_code',
-        code: req.body.code,
-        access_token: [
-            'AA',
-            akConfig.appID,
-            akConfig.appSecret
-        ].join('|')
-    },
-    csrfCheck,
-    req,
-    res,
-    _
-]).map((params, csrfCheck, req, res, _) => [
-    tokenExchangeBaseURL + '?' + Querystring.stringify(params),
-    params,
-    csrfCheck,
-    req,
-    res,
-    _
-]).bind((tokenExchangeURL, params, csrfCheck, req, res, _) => IO.createIO(cb => Request.get({
-    url: tokenExchangeURL,
+]).maybeFalse((csrfCheck, request, response, _) => csrfCheck, (csrfCheck, request, response, _) => respose.end('Something went terribly wrong')).map((csrfCheck, request, response, _) => {
+    Object.defineProperty(params, 'code', {
+        value: request.body.code,
+        enumerable: true,
+        writable: false,
+        configurable: true
+    });
+    return [
+        csrfCheck,
+        request,
+        response,
+        _
+    ];
+}).bind((csrfCheck, request, response, _) => IO.createIO(cb => Request.get({
+    url: tokenExchangeBaseURL + Querystring.stringify(params),
     json: true
-}, cb))).map((tokenExchangeURL, params, csrfCheck, req, res, _, err, resp, respBody) => [
-    {
-        userAccessToken: respBody.access_token,
-        expiresAt: respBody.expires_at,
-        userId: respBody.id
-    },
-    tokenExchangeURL,
-    params,
-    csrfCheck,
-    req,
-    res,
-    _,
-    err,
-    resp,
-    respBody
-]).map((view, tokenExchangeURL, params, csrfCheck, req, res, _, err, resp, respBody) => [
-    meEndpointBaseURL + '?access_token=' + view.userAccessToken,
-    view,
-    tokenExchangeURL,
-    params,
-    csrfCheck,
-    req,
-    res,
-    _,
-    err,
-    resp,
-    respBody
-]).bind((meEndpointURL, view, tokenExchangeURL, params, csrfCheck, req, res, _, err, resp, respBody) => IO.createIO(cb => Request.get({
-    url: meEndpointURL,
+}, cb))).bind((csrfCheck, request, response, _, err, resp, respBody) => IO.createIO(cb => Request.get({
+    url: meEndpointBaseURL + respBody.access_token,
     json: true
-}, cb))).map((meEndpointURL, view, tokenExchangeURL, params, csrfCheck, req, res, _, err, resp, respBody, errURL, respURL, respBodyURL) => {
-    Object.defineProperty(view, 'phoneNumber', {
+}, cb))).map((csrfCheck, request, response, _, err, resp, respBody, errURL, respURL, respBodyURL) => {
+    Object.defineProperty(request.session, 'user', {
         value: respBodyURL.phone.number,
         enumerable: true,
         writable: false,
         configurable: true
     });
     return [
-        meEndpointURL,
-        view,
-        tokenExchangeURL,
-        params,
         csrfCheck,
-        req,
-        res,
+        request,
+        response,
         _,
         err,
         resp,
@@ -116,24 +85,8 @@ IO.createIO(cb => app.post('/login_success', cb)).map((req, res, _) => [
         respURL,
         respBodyURL
     ];
-}).map((meEndpointURL, view, tokenExchangeURL, params, csrfCheck, req, res, _, err, resp, respBody, errURL, respURL, respBodyURL) => [
-    Mustache.to_html(loadLoginSuccess, view),
-    meEndpointURL,
-    view,
-    tokenExchangeURL,
-    params,
-    csrfCheck,
-    req,
-    res,
-    _,
-    err,
-    resp,
-    respBody,
-    errURL,
-    respURL,
-    respBodyURL
-]).then((html, meEndpointURL, view, tokenExchangeURL, params, csrfCheck, req, res, _, err, resp, respBody, errURL, respURL, respBodyURL) => {
-    res.send(html);
+}).then((csrfCheck, request, response, _, err, resp, respBody, errURL, respURL, respBodyURL) => {
+    response.redirect('/');
 });
 IO.createIO(cb => app.get('/logout', cb)).map((request, response, _) => {
     (delete request.session.user)
